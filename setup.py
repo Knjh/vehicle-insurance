326b13180bd0271b771944b3b4a34011ab2d36fd
knjha <kjha@iiitmanipur.ac.in>
2025-02-21 23:07:30 +0530

    project structure ready, venv setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kanhaiya Jha",
+    author_email="knj9304@gmail.com",
+    packages=find_packages()
+)
